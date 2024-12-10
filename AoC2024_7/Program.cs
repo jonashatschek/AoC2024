46d6e4dc458c4ab3c495e83001a54409bf9c9db4
Jonas Hatschek <jonas.hatschek@svea.com>
2024-12-10 17:27:27 +0100

    updated day 7

--- AoC2024_7/Program.cs
+++ AoC2024_7/Program.cs
@@ -1,175 +1,70 @@
-﻿using System.Data;
-using System.Diagnostics.SymbolStore;
-using System.IO.Pipes;
-using System.Linq.Expressions;
-using System.Net.Http.Headers;
-using System.Net.WebSockets;
-using System.Runtime.InteropServices.JavaScript;
-
-namespace AoC2024_7
+﻿namespace AoC2024_7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var test = true;
+            var test = false;
             var path = test ? "Test" : "Real";
             var input = File.ReadAllLines($"{path}/input.txt");
-            var result = new Int128();
+            var p1Result = new long();
+
             foreach (var line in input)
             {
-
                 var splitLine = line.Split(':');
 
                 var id = Int128.Parse(splitLine[0]);
-                var answer = 0;
 
-                var splitNumbers = Array.ConvertAll(splitLine[1].Trim().Split(' '), int.Parse);
+                var splitNumbers = Array.ConvertAll(splitLine[1].Trim().Split(' '), long.Parse);
+                
+                var results = new List<long>();
 
-                char[] operators = ['+', '*'];
-
-
-                var numCombinations = (int)Math.Pow(operators.Length, splitNumbers.Length - 1);
-
-
-                for (int i = 0; i < numCombinations; i++)
+                foreach (var number in splitNumbers)
                 {
-                    string expression = splitNumbers[0].ToString();
-                    var queue = new Queue<string>();
-                    queue.Enqueue(splitNumbers[0].ToString());
-                    int temp = i;
-
-                    if (id == 292)
+                    if (results.Count == 0)
                     {
-                        Console.Write("");
+                        results.Add(number);
+                        continue;
                     }
 
-                    for (int j = 0; j < splitNumbers.Length - 1; j++)
+                    var resultsToAdd = new List<long>();
+
+                    foreach (var result in results)
                     {
-                        char op = operators[temp % operators.Length];
-                        temp /= operators.Length;
+                        var add = result + number;
+                        var mul = result * number;
 
-                        expression += $"{op}{splitNumbers[j + 1]}";
+                        //todo fixa så man kan göra bägge parter
+                        var concat = long.Parse($"{result}{number}");
 
-                        var res = Evaluate(expression, splitNumbers);
-
-                        if (id == res)
+                        if (add <= id)
                         {
-                            result += id;
+                            resultsToAdd.Add(add);
                         }
+
+                        if (mul <= id)
+                        {
+                            resultsToAdd.Add(mul);
+                        }
+
+                        if (concat <= id)
+                        {
+                            resultsToAdd.Add(concat);
+                        }
+
+                        results = resultsToAdd;
                     }
                 }
 
-
-                //todo organize
-                //var calculations = new List<Calculation>();
-
-                //for (int i = 0; i < splitNumbers.Length - 1; i++)
-                //{
-                //    calculations.Add(new Calculation(splitNumbers[i], splitNumbers[i + 1]));
-                //}
-
-                //antalet kombinationer...
-                //for (int i = 0; i < numCombinations; i++)
-                //{
-                //    int temp = i;
-
-                //för varje calculation finns två kombinationer
-                //for (int j = 0; j < calculations.Count; j++)
-                {
-                    //bool op = operators[temp % operators.Length] == 0;
-                    //temp /= operators.Length;
-
-                    //vahvha += op ? calculations[j].AdditionResult : calculations[j].MultiplicationResult;
-
-                    //if (op)
-                    //{
-                    //    vahvha += calculations[j].AdditionResult
-                    //}
-
-                    ////expression += $"{op}{splitNumbers[j + 1]}";
-
-                    ////var res = Evaluate(expression, splitNumbers);
-
-                    //if (id == vahvha)
-                    //{
-                    //    result += id;
-                    //}
-
-                    //var calculations[0]
-                    //}
-
-                    //}
-
-                }
-
-                Console.WriteLine(result);
+                var match = results.FirstOrDefault(x => x == id);
+                p1Result += match;
+                
             }
 
-
-            //public static Int128[] Evaluate(Int128 a, Int128 b, char[] ops)
-            //{
-
-
-            //    var res = new Int128[ops.Length];
-
-            //    for (int i = 0; i < ops.Length; i++)
-            //    {
-            //        var op = ops[i];
-
-            //        DataTable table = new DataTable();
-            //        table.Columns.Add("expression", typeof(string), $"{a}{op}{b}");
-            //        DataRow row = table.NewRow();
-            //        table.Rows.Add(row);
-
-            //        res[i] = Int128.Parse((string)row["expression"]);
-
-            //    }
-
-
-            //    return res;
-
-
-            //    //return Int128.Parse((string)row["expression"]);
-            //}
-
-            public static Int128 Evaluate(string expression, int[] splitNumbers)
-            {
-
-                if (expression == "11 + 6 * 16 + 20")
-                {
-                    Console.Write("ghsa");
-                }
-
-                //var splitWithOperator;
-                //for (int i = 0; i < splitNumbers.Length; i++)s
-                //{
-                //    var indexOfFirstNumber = expression.IndexOf(splitNumbers[i]);
-
-                //    if()
-                //}
-
-
-
-
-                DataTable table = new DataTable();
-                table.Columns.Add("expression", typeof(string), expression);
-                DataRow row = table.NewRow();
-                table.Rows.Add(row);
-                return Int128.Parse((string)row["expression"]);
-            }
+            Console.WriteLine(p1Result);
 
         }
 
-
-    }
-
-    public class Calculation(Int128 a, Int128 b)
-    {
-        public Int128 A { get; set; } = a;
-        public Int128 B { get; set; } = b;
-        public Int128 MultiplicationResult => A * B;
-        public Int128 AdditionResult => A + B;
     }
 
 }
