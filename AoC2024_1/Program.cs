e3ef89fd311edd4627d80336c6ab1137017506ce
joooonas2 <jonas.hatschek@gmail.com>
2024-12-11 15:29:57 +0100

    hello
    agajgad

---
+++ AoC2024_1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC2024_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var historianA = new List<int>();
+			var historianB = new List<int>();
+			
+			var input = File.ReadLines("input.txt");
+
+			foreach (var row in input)
+			{
+				var a = row.Substring(0, 5);
+				var b = row.Substring(5).TrimStart();
+
+				historianA.Add(int.Parse(a));
+				historianB.Add(int.Parse(b));
+
+			}
+
+			historianA.Sort();
+			historianB.Sort();
+
+			var sumDif = 0;
+
+			for (int i = 0; i < historianA.Count; i++)
+			{
+				var difference = historianA[i] - historianB[i];
+
+				sumDif += Math.Abs(difference);
+			}
+
+			var similarityScores = new List<int>();
+
+			foreach(var number in historianA)
+			{
+				var countOccurrences = historianB.Where(x => x == number).Count();
+				similarityScores.Add(number * countOccurrences);
+
+			}
+
+			//p1
+			Console.WriteLine(sumDif);
+
+			//p2
+			Console.WriteLine(similarityScores.Sum());
+		}
+	}
+}
