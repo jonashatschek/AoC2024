ed44471cf0b8cacbd1d140b1a214575b38127d7e
joooonas2 <jonas.hatschek@gmail.com>
2024-12-11 15:15:10 +0100

    eha fa

---
+++ AoC2024_2/hahae.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024_2
+{
+    internal class hahae
+    {
+    }
+}
