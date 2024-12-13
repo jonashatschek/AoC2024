5886a166830becfc8c8d0d35ce4c1d3151bf9bb2
joooonas2 <jonas.hatschek@gmail.com>
2024-12-13 12:39:24 +0100

    rewrote to improve speed of day 9 p1

--- AoC2024_9/Program.cs
+++ AoC2024_9/Program.cs
@@ -1,78 +1,104 @@
-﻿using System.Linq.Expressions;
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Net.Http.Headers;
 
 namespace AoC2024_9
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var test = true;
-            var path = test ? "Test" : "Real";
-            var diskMap = File.ReadAllText($"{path}/input.txt").ToArray();
-            var id = 0;
-            string[] fileBlocks = [];
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var timer = new Stopwatch();
+			timer.Start();
 
-            for (int i = 0; i < diskMap.Length; i++)
-            {
-                fileBlocks = GetNewBlocks(fileBlocks, diskMap[i].ToString(), i, id);
+			
+			var test = false;
+			var path = test ? "Test" : "Real";
+			var diskMap = File.ReadAllText($"{path}/input.txt").ToArray();
+			var id = 0;
+			string[] fileBlocks = [];
+
+			for (int i = 0; i < diskMap.Length; i++)
+			{
+				fileBlocks = GetNewBlocks(fileBlocks, diskMap[i].ToString(), i, id);
 
 				if (i % 2 == 0)
 				{
 					id++;
 				}
-            }
-
-            bool allSorted = false;
-			var indexOfLastInteger = 0;
-
-            //p1
-			while (!allSorted)
-            {
-				var indexOfFirstDot = Array.FindIndex(fileBlocks, x => x == ".");
-				indexOfLastInteger = Array.FindLastIndex(fileBlocks, x => x != ".");
-
-                if(indexOfFirstDot > indexOfLastInteger)
-                {
-                    allSorted = true;
-                }
-                else
-                {
-                    fileBlocks[indexOfFirstDot] = fileBlocks[indexOfLastInteger];
-                    fileBlocks[indexOfLastInteger] = ".";
-                }
 			}
 
-            long checksum = 0;
+			bool allSorted = false;
+			var indexOfLastInteger = fileBlocks.Length - 1;
 
-            for (int i = 0; i < indexOfLastInteger + 1; i++)
-            {
-                long res = long.Parse(fileBlocks[i]) * i;
-                checksum += res;
-            }
+			//p1
+			while (!allSorted)
+			{
+				//slower
+				//var indexOfFirstDot = Array.FindIndex(fileBlocks, x => x == ".");
+				//indexOfLastInteger = Array.FindLastIndex(fileBlocks, x => x != ".");
 
-            Console.WriteLine(checksum);
-        }
 
-        public static string[] GetNewBlocks(string[] oldStuff, string toAdd, int currentIndex, int id)
-        {
-            var toAddLength = int.Parse(toAdd);
-            var newStuff = new string[oldStuff.Length + toAddLength];
+				//faster
+				var indexOfFirstDot = 0;
 
-            if (oldStuff.Length > 0)
-            {
-                oldStuff.CopyTo(newStuff, 0);
-            }
+				for (; indexOfFirstDot < fileBlocks.Length; indexOfFirstDot++)
+				{
+					if (fileBlocks[indexOfFirstDot] == ".")
+					{
+						break;
+					}
+				}
 
-            int whereToStart = oldStuff.Length > 0 ? oldStuff.Length : 0;
+				for (; indexOfLastInteger > 0; indexOfLastInteger--)
+				{
+					if (fileBlocks[indexOfLastInteger] != ".")
+					{
+						break;
+					}
+				}
 
-            for (; whereToStart < newStuff.Length; whereToStart++)
-            {
-                newStuff[whereToStart] += currentIndex % 2 == 0 ? id : ".";
-            }
+				if (indexOfFirstDot > indexOfLastInteger)
+				{
+					allSorted = true;
+				}
+				else
+				{
+					fileBlocks[indexOfFirstDot] = fileBlocks[indexOfLastInteger];
+					fileBlocks[indexOfLastInteger] = ".";
+				}
+			}
 
-            return newStuff;
-        }
+			long checksum = 0;
+
+			for (int i = 0; i < indexOfLastInteger + 1; i++)
+			{
+				checksum += long.Parse(fileBlocks[i]) * i;
+			}
+
+			Console.WriteLine($"Part1 time: {timer.ElapsedMilliseconds}");
+			Console.WriteLine(checksum);
+		}
+
+		public static string[] GetNewBlocks(string[] oldStuff, string toAdd, int currentIndex, int id)
+		{
+			var toAddLength = int.Parse(toAdd);
+			var newStuff = new string[oldStuff.Length + toAddLength];
+
+			if (oldStuff.Length > 0)
+			{
+				oldStuff.CopyTo(newStuff, 0);
+			}
+
+			int whereToStart = oldStuff.Length > 0 ? oldStuff.Length : 0;
+
+			for (; whereToStart < newStuff.Length; whereToStart++)
+			{
+				newStuff[whereToStart] += currentIndex % 2 == 0 ? id : ".";
+			}
+
+			return newStuff;
+		}
 
 	}
 
