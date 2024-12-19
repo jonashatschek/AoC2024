734b52f0fa6e3654d28343a28093c26abd0f3722
Jonas Hatschek <jonas.hatschek@svea.com>
2024-12-19 10:58:45 +0100

    minor updates to day 10. started on day 12

--- AoC2024_12/Program.cs
+++ AoC2024_12/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace AoC2024_12
+﻿using System.Diagnostics;
+
+namespace AoC2024_12
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+            var timer = new Stopwatch();
+            timer.Start();
+            var useTestData = false;
+            var path = useTestData ? "Test" : "Real";
+            var farm = File.ReadAllLines($"{path}/input.txt");
+
+
+            var crops = new List<Crop>();
+            //count how many of each crop is in the farm
+            var farmCount = new Dictionary<char, int>();
+
+            var cols = farm.Length;
+            for (int y = 0; y < cols; y++)
+            {
+                var row = farm[y].Length;
+
+                for (int x = 0; x < row; x++)
+                {
+                    var crop = new Crop();
+
+                }
+            }
+
+
+            //File.ReadAllText($"{path}/input.txt").Split(' ').Select(long.Parse).ToList()
+
+
+        }
+
+
 	}
+
+    public class Crop
+    {
+        public char Type { get; set; }
+        public int Perimiter { get; set; }
+        public int Area { get; set; }
+    }
 }
